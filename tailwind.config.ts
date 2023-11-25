cf95cff8c3b83e0e069f327d3c280b3ad1139620
zoubair_djebah <zoubairdjebah.dev@gmail.com>
2023-11-25 20:25:05 +0100

    feature(color): color code setup in Tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      white: "#FFFFFF",
+      primary: {
+        200: "#EFEFFC",
+        300: "#CFD0F6",
+        400: "#7F81E8",
+        DEFAULT: "#5F62E2",
+        600: "#5658CB",
+      },      
+      secondary: {
+        200: "#EAF8F4",
+        300: "#BFE9DE",
+        400: "#56C4A7",
+        DEFAULT: "#2AB691",
+        600: "#26A482",
       },
+      gray: {
+        400: "#F2F2F2",
+        500: "#E5E5E5",
+        600: "#B2B2B2",
+        700: "#808080",
+        800: "#333333",
+        DEFAULT: "#1D1D1D",
+      },
+
     },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
