ad072910d1d083a7aeacaca57d5f8a030e9de7aa
zoubair_djebah <zoubairdjebah.dev@gmail.com>
2023-11-25 20:50:26 +0100

    feature(text style): tailwind CSS configuration for personalized text style customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
         400: "#7F81E8",
         DEFAULT: "#5F62E2",
         600: "#5658CB",
-      },      
+      },
       secondary: {
         200: "#EAF8F4",
         300: "#BFE9DE",
@@ -27,8 +27,122 @@ const config: Config = {
         800: "#333333",
         DEFAULT: "#1D1D1D",
       },
-
     },
+    fontSize: {
+      "8xl": /* Display - 120/120 */ [
+        "120px",
+        {
+          lineHeight: "120px",
+          letterSpacing: "-6px",
+          fontWeight: "500",
+        },
+      ],
+      "7xl": /* Headline 1 - 72/80 */ [
+        "72px",
+        {
+          lineHeight: "80px",
+          letterSpacing: "-4.5px",
+          fontWeight: "600",
+        },
+      ],
+      "6xl": /* Headline 2 - 55/60 */ [
+        "55px",
+        {
+          lineHeight: "60px",
+          letterSpacing: "-2.5px",
+          fontWeight: "500",
+        },
+      ],
+      "5xl": /* Headline 3 - 48/54 */ [
+        "48px",
+        {
+          lineHeight: "54px",
+          letterSpacing: "-1.6px",
+          fontWeight: "500",
+        },
+      ],
+      "4xl": /* Headline 4 - 36/44 */ [
+        "36px",
+        {
+          lineHeight: "44px",
+          letterSpacing: "-1.2px",
+          fontWeight: "500",
+        },
+      ],
+      "3xl": /* Headline 5 - 28/34 */ [
+        "28px",
+        {
+          lineHeight: "34px",
+          letterSpacing: "-0.8px",
+          fontWeight: "500",
+        },
+      ],
+      "2xl": /* Lead - 24/30 */ [
+        "24px",
+        {
+          lineHeight: "30px",
+          letterSpacing: "-1px",
+          fontWeight: "400",
+        },
+      ],
+      lg: /* Body ld - 21/30 */ [
+        "21px",
+        {
+          lineHeight: "30px",
+          letterSpacing: "-0.8px",
+          fontWeight: "400",
+        },
+      ],
+      base: /* Body base - 17/25 */ [
+        "17px",
+        {
+          lineHeight: "25px",
+          letterSpacing: "-0.7px",
+          fontWeight: "400",
+        },
+      ],
+      sm: /* Body sm - 15/23 */ [
+        "15px",
+        {
+          lineHeight: "23px",
+          letterSpacing: "-0.6px",
+          fontWeight: "400",
+        },
+      ],
+      caption1: /* caption 1 - 20/24 */ [
+        "20px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "-0.6px",
+          fontWeight: "400",
+        },
+      ],
+      caption2: /* caption 2 - 18/20 */ [
+        "18px",
+        {
+          lineHeight: "20px",
+          letterSpacing: "-0.3px",
+          fontWeight: "400",
+        },
+      ],
+      caption3: /* caption 3 - 16/18 */ [
+        "16px",
+        {
+          lineHeight: "18px",
+          letterSpacing: "-0.5px",
+          fontWeight: "400",
+        },
+      ],
+      caption4: /* caption 4 - 13/15 */ [
+        "13px",
+        {
+          lineHeight: "15px",
+          letterSpacing: "-0.2px",
+          fontWeight: "400",
+        },
+      ],
+    },
+
     extend: {},
   },
   plugins: [],
